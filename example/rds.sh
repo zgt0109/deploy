8f3c5b4f0c3da5e23d8e6899993f5ca51c999a36
zouguotao <2506526350@qq.com>
2019-03-20 16:24:25 +0800

    update deploy bundler version

--- example/rds.sh
+++ example/rds.sh
@@ -5,24 +5,7 @@ export LC_ALL="en_US.UTF-8"
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get install -yq  --no-install-recommends \
-        build-essential language-pack-zh-hans git-core tig nginx libcurl4-openssl-dev libcurl3
-
-# nginx
-sed  -i 's/#\s\+\(server_names_hash_bucket_size\)/\1/' /etc/nginx/nginx.conf
-
-# 前端安装nodejs 和 yarn
-wget -qO- https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get update && apt-get install -y nodejs
-
-## To install the Yarn package manager, run:
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install yarn
-
-# yarn 安装源
-yarn config set registry 'https://registry.npm.taobao.org'
-# node-sass 解析器
-yarn config set sass_binary_site "http://cdn.npm.taobao.org/dist/node-sass"
+        build-essential language-pack-zh-hans git-core tig libcurl4-openssl-dev libcurl3
 
 # install ruby
 # https://www.brightbox.com/docs/ruby/ubuntu/
@@ -37,20 +20,8 @@ gem sources --add $GEM_SOURCES_CHINA --remove $GEM_SOURCES_ORIGIN -v
 echo 'gem: --no-document' | tee -a ~/.gemrc
 
 # bundler
-gem install bundler -v 1.17.1
+gem install bundler
 bundle config mirror.${GEM_SOURCES_ORIGIN%/} ${GEM_SOURCES_CHINA%/}
-
-# install mysql and redis
-apt-get install -yq mysql-server mysql-client libmysqlclient-dev
-apt-get install -yq redis-server redis-tools
-
-# 解决 Ubuntu 16.04 上mysql5.7不能直接用 root 用户登录
-# eg  medusa_client_production
-read -p "Production Database Name? " database_name
-mysql -u root -e "create database if not exists $database_name default charset utf8;"
-mysql -u root -e "update mysql.user set plugin='mysql_native_password' WHERE User='root';"
-mysql -u root -e "update mysql.user set authentication_string=password('thrive') WHERE User='root';"
-service mysql restart
 
 # 创建后端部署用户和APP名称
 read -p "Deploy User Name? " user_name
