fa7a03386293ab6451820b986b248d44f310d10d
zgt <2506526350@qq.com>
2020-09-09 14:29:42 +0800

    update install.sh

--- example/install.sh
+++ example/install.sh
@@ -5,7 +5,7 @@ export LC_ALL="en_US.UTF-8"
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get install -yq  --no-install-recommends \
-        build-essential language-pack-zh-hans git-core tig nginx libcurl4-openssl-dev libcurl3
+        build-essential language-pack-zh-hans git tig nginx libcurl4-openssl-dev libcurl3
 
 # nginx
 sed  -i 's/#\s\+\(server_names_hash_bucket_size\)/\1/' /etc/nginx/nginx.conf
@@ -14,25 +14,15 @@ sed  -i 's/#\s\+\(server_names_hash_bucket_size\)/\1/' /etc/nginx/nginx.conf
 wget -qO- https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get update && apt-get install -y nodejs
 
-## To install the Yarn package manager, run:
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install yarn
-
-# yarn 安装源
-yarn config set registry 'https://registry.npm.taobao.org'
-# node-sass 解析器
-yarn config set sass_binary_site "http://cdn.npm.taobao.org/dist/node-sass"
-
 # install ruby
 # https://www.brightbox.com/docs/ruby/ubuntu/
 apt-get install -yq software-properties-common
 apt-add-repository ppa:brightbox/ruby-ng
 
-apt-get update && apt-get install -yq  --no-install-recommends ruby2.5 ruby2.5-dev
+apt-get update && apt-get install -yq  --no-install-recommends ruby2.6 ruby2.6-dev
 # 设置项目所需ruby的正确版本
 apt-get install ruby-switch
-ruby-switch --set ruby2.5
+ruby-switch --set ruby2.6
 
 # Gem
 GEM_SOURCES_CHINA=https://gems.ruby-china.com/
